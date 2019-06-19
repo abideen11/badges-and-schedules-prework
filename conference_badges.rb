2910150d599987574fd9ea52fbed2e8155b572c8
Abideen Kareem <abideenkareem17@gmail.com>
2019-06-19 22:36:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+def batch_badge_creator(name_array)
+  name_array.each do |n|
+  "Hello, my name is: #{n}"
+end
+end 
